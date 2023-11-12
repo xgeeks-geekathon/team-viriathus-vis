2080bd3b79834024b9cab6510b6b9aac1a033389
Helder Santos <helder.santos@cazoo.co.uk>
2023-11-12 12:11:22 +0000

    Fix prompt borders

--- src/components/PromptInput.tsx
+++ src/components/PromptInput.tsx
@@ -60,12 +60,12 @@ export const PromptInput = ({
         value={selectedPrompt}
         onChange={onChange}
         onKeyDown={onKeyDown}
-        className="w-full text-black flex-auto rounded-t-md border-0 px-3.5 py-2 shadow-sm focus:outline-none  sm:text-sm sm:leading-6"
+        className={`w-full text-black flex-auto rounded-t-md ${suggestedPrompts.length > 0 ? '' : 'rounded-b-md'} border-0 px-3.5 py-2 shadow-sm focus:outline-none  sm:text-sm sm:leading-6`}
         placeholder="Start typing to get prompt suggestions"
         disabled={disabled}
       />
       {suggestedPrompts.length > 0 && (
-        <div className="absolute bg-gray-800 z-50 w-full p-2 rounded-md border-0 text-white shadow-sm ring-1 ring-inset ring-white/10">
+        <div className="absolute bg-gray-800 z-50 w-full p-2 rounded-b-md border-0 text-white shadow-sm ring-1 ring-inset ring-white/10">
           <div className="max-h-80 overflow-auto">
             {suggestedPrompts.slice(0, 10).map((prompt) => (
               <button
